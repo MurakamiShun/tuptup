f6775ffa97543ee1e80d35063ffc83b623839657
akisute514 <akisute514@gmail.com>
2021-09-12 07:53:35 +0900

    complete apply each

--- include/tuptup/apply.hpp
+++ include/tuptup/apply.hpp
@@ -13,7 +13,7 @@ namespace tuptup {
     namespace detail {
         template<typename F, typename Tuple, std::size_t... I>
         decltype(auto) apply_impl(F&& f, Tuple&& tup, integer_sequence<std::size_t, I...>&&) {
-            return f(std::get<I>(tup)...);
+            return f(std::get<I>(std::forward<Tuple>(tup))...);
         }
     }
 
@@ -25,8 +25,8 @@ namespace tuptup {
     namespace detail {
         template<typename F, typename Tuple, std::size_t... I>
         auto apply_impl(F&& f, Tuple&& tup, integer_sequence<std::size_t, I...>&&)
-            -> decltype(f(std::get<I>(tup)...)){
-            return f(std::get<I>(tup)...);
+            -> decltype(f(std::get<I>(std::forward<Tuple>(tup))...)){
+            return f(std::get<I>(std::forward<Tuple>(tup))...);
         }
     }
 
@@ -41,42 +41,72 @@ namespace tuptup {
         struct apply_each_impl;
         template<typename F, typename T, std::size_t... I>
         struct apply_each_impl<F, T, integer_sequence<std::size_t, I...>> {
+            template<typename ElmType>
+            using make_forward_type = typename std::conditional<std::is_lvalue_reference<T>::value,
+                typename std::add_lvalue_reference<ElmType>::type,
+                typename std::add_rvalue_reference<ElmType>::type
+            >::type;
 
-            constexpr static void call_all(F&& f){}
-            template<typename Head, typename... Tails>
-            constexpr static void call_all(F&& f, Head&& head, Tails&&... tails){
-                f(std::forward<Head>(head));
-                call_all(std::forward<F>(f), std::forward<Tails>(tails)...);
+            using Tuple = typename std::remove_reference<T>::type;
+
+            #if __cplusplus >= 201703L
+            template<typename... ArgTypes>
+            using invoke_result_t = std::invoke_result_t<F, ArgTypes...>;
+            #else
+            template<typename... Args>
+            using invoke_result_t = typename std::result_of<F(Args...)>::type;
+            #endif
+            template<typename Damy, typename... Elms>
+            struct call_result;
+            template<typename Damy, typename Head, typename... Elms>
+            struct call_result<Damy, Head, Elms...>{
+                using type = tuple_cat_t<typename std::conditional<
+                    std::is_same<invoke_result_t<Head>, void>::value,
+                    std::tuple<>,
+                    std::tuple<invoke_result_t<Head>>
+                >::type, typename call_result<void, Elms...>::type>;
+            };
+            template<typename Damy>
+            struct call_result<Damy>{
+                using type = std::tuple<>;
+            };
+
+            using call_result_t = typename call_result<void, make_forward_type<typename std::tuple_element<I, Tuple>::type>...>::type;
+
+            constexpr static std::tuple<> call_all(F& f){ return std::tuple<>{}; }
+            template<typename Head,
+                typename std::enable_if<std::is_same<invoke_result_t<Head>, void>::value, std::nullptr_t>::type = nullptr,
+                typename... Tails
+            >
+            constexpr static auto call_all(F& f, Head&& head, Tails&&... tails)
+                -> typename call_result<void, Head, Tails...>::type {
+                return (f(std::forward<Head>(head)), call_all(f, std::forward<Tails>(tails)...));
+            }
+            template<typename Head,
+                typename std::enable_if<!std::is_same<invoke_result_t<Head>, void>::value, std::nullptr_t>::type = nullptr,
+                typename... Tails
+            >
+            constexpr static auto call_all(F& f, Head&& head, Tails&&... tails)
+                -> typename call_result<void, Head, Tails...>::type {
+                return tuple_cat(
+                    make_tuple(f(std::forward<Head>(head))),
+                    call_all(f, std::forward<Tails>(tails)...)
+                );
             }
 
-            /*
-            template<std::size_t N>
-            using call_result_t = decltype(std::declval<F>()(std::declval<typename std::tuple_element<N, T>::type>()));
-
-            template<template<typename...>class TupleType, typename... Elms>
-            auto operator()(F&& f, TupleType<Elms...>&& t) const
-                -> decltype(std::declval<TupleType<call_result_t<I>...>>()){
-                //call_all(std::forward<F>(f), std::get<I>(std::forward<TupleType<Elms...>&&>(t))...);
-            }
-            template<template<typename...>class TupleType, typename... Elms>
-            auto operator()(F&& f, TupleType<Elms...>& t) const
-                -> decltype(std::declval<TupleType<call_result_t<I>...>>()){
-                //call_all(std::forward<F>(f), std::get<I>(std::forward<decltype(t)>(t))...);
-            }
-            */
-
-            void operator()(F&& f, T&& t) const {
-                call_all(std::forward<F>(f), std::get<I>(std::forward<decltype(t)>(t))...);
+            template<typename Tup>
+            constexpr auto operator()(F& f, Tup&& t) const -> call_result_t {
+                return call_all(f, std::get<I>(std::forward<decltype(t)>(t))...);
             }
         };
     }
     template<typename F, typename T>
     constexpr auto apply_each(F&& f, T&& tup)
         -> decltype(detail::apply_each_impl<
-            F, T,
-            make_integer_sequence<std::size_t, tuple_size<typename std::remove_reference<T>::type>::value>>{}(std::forward<decltype(f)>(f), std::forward<decltype(tup)>(tup))) {
+            typename std::remove_reference<F>::type, T,
+            make_integer_sequence<std::size_t, tuple_size<typename std::remove_reference<T>::type>::value>>{}(f, std::forward<T>(tup))) {
         return detail::apply_each_impl<
-            F, T,
-            make_integer_sequence<std::size_t, tuple_size<typename std::remove_reference<T>::type>::value>>{}(std::forward<decltype(f)>(f), std::forward<decltype(tup)>(tup));
+            typename std::remove_reference<F>::type, T,
+            make_integer_sequence<std::size_t, tuple_size<typename std::remove_reference<T>::type>::value>>{}(f, std::forward<T>(tup));
     }
 }
--- tests/apply_each.cpp
+++ tests/apply_each.cpp
@@ -18,4 +18,18 @@ int main(){
     tuptup::apply_each(functor, sii);
 
     assert(functor.oss.str() == "string:testint:5int:6");
+    
+    struct {
+        void operator()(const std::string& str){}
+        int64_t operator()(int& i){
+            auto tmp = i;
+            i = i*2;
+            return tmp;
+        }
+    } square_if_int;
+
+    auto before = tuptup::apply_each(square_if_int, sii);
+
+    assert((before == std::tuple<int64_t, int64_t>{5,6}));
+    assert((sii == decltype(sii){"test",10,12}));
 }
