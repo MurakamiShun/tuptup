42ce9410d52a7d0a715eeba263985dca8d4c7171
akisute514 <akisute514@gmail.com>
2021-09-13 09:34:02 +0900

    fix test bug

--- tests/indexed_apply.cpp
+++ tests/indexed_apply.cpp
@@ -1,23 +1,21 @@
 #include <sstream>
-#include <cassert>
-#include <iostream>
 #include <tuptup.hpp>
+#include <cassert>
 
 struct func{
     std::ostringstream oss;
     template<std::size_t N>
     std::string operator()(const std::string& str){
-        oss << N << "-string:" << str;
+        oss << "string-" << N << ":" << str;
         return "eeee";
     }
     template<std::size_t N>
-    int64_t operator()(const int8_t i){
-        oss << N << "-int:" << i;
-        return 99;
+    void operator()(const int8_t i){
+        oss << "int8-"  << N << ":" << static_cast<int>(i);
     }
     template<std::size_t N>
     int8_t operator()(const int64_t i){
-        oss << N << "-int:" << i;
+        oss << "int64-"  << N << ":" << i;
         return 99;
     }
 };
@@ -29,6 +27,6 @@ int main(){
 
     auto result = tuptup::indexed_apply_each(functor, sii);
 
-    assert((functor.oss.str() == "0-string:test1-int:2-int:6"));
-    assert((std::is_same<decltype(result), std::tuple<std::string, int64_t, int8_t>>::value));
+    assert((functor.oss.str() == "string-0:testint8-1:5int64-2:6"));
+    assert((std::is_same<decltype(result), std::tuple<std::string, int8_t>>::value));
 }
