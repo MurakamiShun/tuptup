503a2ff5fe8752cb60a674b3d42f84d86751d346
akisute514 <akisute514@gmail.com>
2022-05-02 01:16:08 +0900

    fix

--- include/tuptup/struct_binder.hpp
+++ include/tuptup/struct_binder.hpp
@@ -1604,32 +1604,35 @@ namespace tuptup{
         return struct_binder<T>{}(t);
     }
 
-    template<typename T>
-    static auto make_tuple_from_struct(T&& t){
-        struct {
+    namespace detail{
+        struct make_tuple_from_struct_impl{
             template<typename Tup, std::size_t... I>
-            auto make_tuple_by_move_impl(Tup& tup, std::index_sequence<I...>){
-                return std::make_tuple(std::move(std::get<I>(t)...));
+            static auto make_tuple_by_move_impl(Tup& tup, std::index_sequence<I...>){
+                return std::make_tuple(std::move(std::get<I>(tup)...));
             }
             template<typename Tup>
-            auto make_tuple_by_move(Tup& tup) {
+            static auto make_tuple_by_move(Tup& tup) {
                 return make_tuple_by_move_impl(tup, std::make_index_sequence<std::tuple_size_v<Tup>>{});
             }
             template<typename Tup, std::size_t... I>
-            auto make_tuple_impl(Tup& tup, std::index_sequence<I...>){
-                return std::make_tuple(std::get<I>(t)...);
+            static auto make_tuple_impl(Tup& tup, std::index_sequence<I...>){
+                return std::make_tuple(std::get<I>(tup)...);
             }
             template<typename Tup>
-            auto make_tuple(Tup& tup) {
+            static auto make_tuple(Tup& tup) {
                 return make_tuple_impl(tup, std::make_index_sequence<std::tuple_size_v<Tup>>{});
             }
-        } tuple_utils;
+        };
+    }
+
+    template<typename T>
+    static auto make_tuple_from_struct(T&& t){
         auto member_ref_tuple = struct_binder<std::remove_reference_t<T>>{}(t);
         if constexpr(std::is_rvalue_reference_v<T>){
-            return tuple_utils.make_tuple_by_move(member_ref_tuple);
+            return detail::make_tuple_from_struct_impl::make_tuple_by_move(member_ref_tuple);
         }
         else{
-            return tuple_utils.make_tuple(member_ref_tuple);
+            return detail::make_tuple_from_struct_impl::make_tuple(member_ref_tuple);
         }
     }
 }
